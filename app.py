128016db116670a6de4f5875d3c714395b02cb4a
maplemaybe <76513373+maplemaybe@users.noreply.github.com>
2021-10-06 11:43:16 +0800

    test

--- app.py
+++ app.py
@@ -36,5 +36,5 @@ def handle_message(event):
     get_message = event.message.text
 
     # Send To Line
-    reply = TextSendMessage(text=f"{get_message}")
+    reply = TextSendMessage(text= "你說的是不是："+ f"{get_message}")
     line_bot_api.reply_message(event.reply_token, reply)
