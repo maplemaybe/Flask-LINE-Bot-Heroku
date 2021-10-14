d36dcf855a2e2a97bcae23e3e76b0a3a7cd36c3d
maplemaybe <76513373+maplemaybe@users.noreply.github.com>
2021-10-14 21:57:34 +0800

    data

--- app.py
+++ app.py
@@ -37,16 +37,16 @@ def handle_message(event):
     get_message = event.message.text
 
     # Send To Line
-    #reply_arr=[]
     #reply = TextSendMessage(text= "你說的是不是："+ f"{get_message}")
     #line_bot_api.reply_message(event.reply_token, reply)
+    DATABASE_URL = os.environ['DATABASE_URL']
+    #conn = psycopg2.connect(DATABASE_URL, sslmode='require')
+    reply = TextSendMessage(text=DATABASE_URL)
+    line_bot_api.reply_message(event.reply_token, reply)   
 
-    record_list = prepare_record(get_message)
-    reply_list = TextSendMessage(text=line_insert_record(record_list))
+    #record_list = prepare_record(get_message)
+    #reply_list = TextSendMessage(text=line_insert_record(record_list))
     #line_bot_api.reply_message(event.reply_token, reply_list)
-    #reply_arr.append(reply)
-    #reply_arr.append(reply_list)
-    line_bot_api.reply_message(event.reply_token, reply_list)
 
 
 def prepare_record(text):
