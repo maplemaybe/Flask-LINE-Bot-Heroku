e23572665652542468dcc0da0f4885adfd3a8ee2
maplemaybe <76513373+maplemaybe@users.noreply.github.com>
2021-10-16 15:54:01 +0800

    UP

--- app.py
+++ app.py
@@ -37,9 +37,9 @@ def handle_message(event):
     get_message = event.message.text
 
     # Send To Line
-    reply = TextSendMessage(text= "你說的是不是："+ f"{get_message}")
-    line_bot_api.reply_message(event.reply_token, reply)
-    
+    #reply = TextSendMessage(text= "你說的是不是："+ f"{get_message}")
+    #line_bot_api.reply_message(event.reply_token, reply)
+
     if '草泥馬訓練紀錄' in event.message.text:
         
         try:
